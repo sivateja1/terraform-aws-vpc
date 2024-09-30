95eb58e377efc78063f1f1d7646fd9ea46d46fef
Sannidhi Siva Teja <sivateja152@gmail.com>
2024-09-30 14:32:48 +0530

    Update peering.tf

--- peering.tf
+++ peering.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "peering" {
-    count = var.is_peering_requireed ? 1 : 0
+    count = var.is_peering_required ? 1 : 0
     peer_vpc_id = data.aws_vpc.default.id #acceptor
     vpc_id = aws_vpc.main.id #requestor
     
@@ -15,7 +15,7 @@ resource "aws_vpc_peering_connection" "peering" {
 }
 
 resource "aws_route" "public_peering" {
-    count = var.is_peering_requireed ? 1 : 0
+    count = var.is_peering_required ? 1 : 0
     route_table_id = aws_route_table.public.id
     destination_cidr_block = data.aws_vpc.default.cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
@@ -29,14 +29,14 @@ resource "aws_route" "private_peering" {
 }
 
 resource "aws_route" "database_peering" {
-    count = var.is_peering_requireed ? 1 : 0
+    count = var.is_peering_required ? 1 : 0
     route_table_id = aws_route_table.database.id
     destination_cidr_block = data.aws_vpc.default.cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
 }
 
 resource "aws_route" "default_peering" {
-    count = var.is_peering_requireed ? 1 : 0
+    count = var.is_peering_required ? 1 : 0
     route_table_id = data.aws_route_table.main.route_table_id
     destination_cidr_block = var.vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
